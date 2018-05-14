c35da0ee150caa187184f8df339a4b88d4b4e571
Kassandra Keeton <ProsperousHeart@users.noreply.github.com>
2018-05-13 23:18:41 -0500

    Updating of the app
    As learning more, updating this app - just completed RESTful and about to do DB work

--- Starter Apps/REST-APIs/02 - Flask RESTful/code/app.py
+++ Starter Apps/REST-APIs/02 - Flask RESTful/code/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 # RESOURCE:  represents an item an API can return or create
 #   usually mapped into DB tables as well
 
@@ -28,6 +28,14 @@ items = []
 
 # API works with resources, & every resource has to be a class
 class Item(Resource): # Item inherits from class Resource (flask_restful)
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank and must be a float!"
+    )
+    
     @jwt_required()
     def get(self, name):
         """
@@ -56,6 +64,7 @@ class Item(Resource): # Item inherits from class Resource (flask_restful)
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
 
+    @jwt_required()
     def post(self, name):
         """
         This will add a new item - currently does not check for duplicates.
@@ -71,11 +80,35 @@ class Item(Resource): # Item inherits from class Resource (flask_restful)
             """
             return {'message': "An item with name '{}' already exists!".format(name)}, 400
 
-        data = request.get_json()   # will give an error if improper type or content
+        # data = request.get_json()   # will give an error if improper type or content
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return items, 201    # tells app liek POSTMAN that successful, 201 - CREATED
         # 202 - ACCEPTED (when delaying the creation ... such as if it takes a long time)
+
+    @jwt_required()
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message':  "Item '{}' deleted.".format(name)}
+
+    @jwt_required()
+    def put(self, name):
+        
+        global items
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        
+        # data = request.get_json()
+        data = Item.parser.parse_args()
+
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
 
 class ItemList(Resource):
     def get(self):
